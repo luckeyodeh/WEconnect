c8a0d8c195582ebc0fb4831d8d7bdaee3f09bed8
ODEH <luckeyodeh@gmail.com>
2018-03-23 13:22:58 +0100

    Users auth setup

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -52,4 +52,25 @@ export default class Middleware {
 
     next();
   }
+
+  /**
+   * Checks if a user is logged in
+   * @param {object} request The requestuest body of the requestuest.
+   * @param {object} response The responseponse body.
+   * @param {object} next Passes control to next middleware
+   * @returns {object} next
+   */
+  static auth(request, response, next) {
+    const token = request.body.token || request.query.token || request.headers['x-access-token'];
+    jwt.verify(token, process.env.SALT, (err, decoded) => {
+      if (err) {
+        return response.status(401).json({
+          error: true,
+          message: 'User not logged in'
+        });
+      }
+      request.userId = decoded.id;
+      return next();
+    });
+  }
 }
